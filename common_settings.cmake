455eef3ec20a9add0ebe8c945bf4f3d91425db4f
Isaac Robinson <isaacrob@me.com>
2015-07-16 16:56:27 -0400

    started configuration for laptop

--- common_settings.cmake
+++ common_settings.cmake
@@ -339,11 +339,12 @@ elseif(HOSTED_AT_D2_NO_GPU GREATER -1)
   set(liblinear_INCLUDE_DIRS "/home/rodrigob/code/references/liblinear-1.8")
   set(liblinear_LIBRARY_DIRS "/home/rodrigob/code/references/liblinear-1.8")
 
-elseif(${HOSTNAME} STREQUAL  "the_name_of_your_machine")
+elseif(${HOSTNAME} STREQUAL  "uold-newton")
   # change the_name_of_your_machine to what /bin/hostname returns
 
   message(STATUS "Using the_name_of_your_machine compilation options")
   # start with an empty section, and see what fails as you go through the readme.text instructions
+  set(Boost_INCLUDE_DIR "/home/isaac/doppia/libs/boost")
 
 else ()
   message(FATAL_ERROR, "Unknown machine, please add your configuration inside doppia/common_settings.cmake")
